d99002e4deb4fcd0528c81e25108be38e6ec8e96
Jelle Luteijn <jelle@luteijn.pro>
2016-09-08 13:17:58 +0200

    view models

--- src/DeKoningAutoverhuur.Web.WebApplication/Models/ReserveViewModels/DetailsViewModel.cs
+++ src/DeKoningAutoverhuur.Web.WebApplication/Models/ReserveViewModels/DetailsViewModel.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DeKoningAutoverhuur.Web.WebApplication.Models.ReserveVielModels
+namespace DeKoningAutoverhuur.Web.WebApplication.Models.ReserveViewModels
 {
-    public class ReserveViewModel
+    public class DetailsViewModel
     {
         public string companyname { get; set; }
         public string vat { get; set; }
