524d8eb2970e50be2af86171bfbc74942f642268
Jelle Luteijn <jelle@luteijn.pro>
2016-09-08 14:41:38 +0200

    details into session

--- src/DeKoningAutoverhuur.Web.WebApplication/Controllers/ReserveController.cs
+++ src/DeKoningAutoverhuur.Web.WebApplication/Controllers/ReserveController.cs
@@ -161,64 +161,23 @@ namespace DeKoningAutoverhuur.Web.WebApplication.Controllers
         // GET: /<controller>/
         public IActionResult Details()
         {
-            List<CarObject> cars = new List<CarObject>();
-            CarObject car = new CarObject();
-            car.LicensePlate = "12-34-AB";
-            car.Brand = "Mercedes";
-            car.Model = "A";
-            car.Transmission = "Manual";
-            car.Segment = "A";
-            cars.Add(car);
-
-            ViewData["cars"] = cars;
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Details(DetailsViewModel model)
         {
-            List<CarObject> cars = new List<CarObject>();
-            CarObject car = new CarObject();
-            car.LicensePlate = "12-34-AB";
-            car.Brand = "Mercedes";
-            car.Model = "A";
-            car.Transmission = "Manual";
-            car.Segment = "A";
-            cars.Add(car);
-
-            ViewData["cars"] = cars;
-
-            string messageText = @"Er is een nieuwe reservering voor " + model.fromdatetime + @".
-Gegevens Klant
-Naam = " + model.name_firstname + @" " + model.name_prefix + @" " + model.name_lastname + @"
-Adres = " + model.address_street + @" " + model.address_number + @" " + model.address_suffix + @"
-postcode = " + model.address_zipcode + @"
-city = " + model.address_city + @"
-country = " + model.address_country + @"
-phone = " + model.phone_countrycode + @" " + model.phone_areacode + @" " + model.phone_subscribernumber + @"
-
-Gewenste Auto
-car = " + model.car + @"";
-
-            var message = new MimeMessage();
-
-            message.To.Add(new MailboxAddress("receptie", "receptie@koningverhuur.auto"));
-
-            message.From.Add(new MailboxAddress("Website", "noreply@koningverhuur.auto"));
-            message.Subject = "Nieuwe reservering";
-            message.Body = new TextPart("plain")
-            {
-                Text = messageText
-            };
-
-            using (var client = new SmtpClient())
-            {
-                client.Connect("localhost", 25, false);
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-                client.Send(message);
-                client.Disconnect(true);
-            }
-            return View("~/Views/Reserve/IndexSuccess.cshtml");
+            HttpContext.Session.SetString("name_initials", model.name_initials);
+            HttpContext.Session.SetString("name_firstname", model.name_firstname);
+            HttpContext.Session.SetString("name_prefix", model.name_prefix);
+            HttpContext.Session.SetString("name_lastname", model.name_lastname);
+            HttpContext.Session.SetString("phone_countrycode", model.phone_countrycode);
+            HttpContext.Session.SetString("phone_areacode", model.phone_areacode);
+            HttpContext.Session.SetString("phone_subscribernumber", model.phone_subscribernumber);
+            HttpContext.Session.SetString("email", model.email);
+            HttpContext.Session.SetString("address_country", model.address_country);
+            HttpContext.Response.Redirect("/reserve/payment");
+            return View("~/Views/Shared/_Redirect.cshtml");
         }
 
         // GET: /<controller>/
