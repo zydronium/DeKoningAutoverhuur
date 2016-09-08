4450507d9e3345990217e8f3caa5b1dc9df98eea
Jelle Luteijn <jelle@luteijn.pro>
2016-09-08 13:52:48 +0200

    add image to carmode object

--- src/DeKoningAutoverhuur.Web.WebApplication/Common/Objects/CarModelObject.cs
+++ src/DeKoningAutoverhuur.Web.WebApplication/Common/Objects/CarModelObject.cs
@@ -13,6 +13,7 @@ namespace DeKoningAutoverhuur.Web.WebApplication.Common.Objects
         public string Transmission;
         public string Segment;
         public double PricePerDay;
+        public string image;
         public DateTime Created;
         public DateTime Modified;
         public Boolean Deleted;
