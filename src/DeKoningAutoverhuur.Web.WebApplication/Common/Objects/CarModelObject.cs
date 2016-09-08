ae10b730951d0933abbc3f7a4c51c00b525de983
Jelle Luteijn <jelle@luteijn.pro>
2016-09-08 13:45:55 +0200

    CarModel object

---
+++ src/DeKoningAutoverhuur.Web.WebApplication/Common/Objects/CarModelObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeKoningAutoverhuur.Web.WebApplication.Common.Objects
+{
+    public class CarModelObject
+    {
+        public string Id;
+        public string Brand;
+        public string Model;
+        public string Transmission;
+        public string Segment;
+        public double PricePerDay;
+        public DateTime Created;
+        public DateTime Modified;
+        public Boolean Deleted;
+        public DateTime DeletedDate;
+    }
+}
