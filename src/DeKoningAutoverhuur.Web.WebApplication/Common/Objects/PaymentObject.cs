7c899735bd4aeded9598a9556bf9f1985cfd96a6
Jelle Luteijn <jelle@luteijn.pro>
2016-09-08 14:42:30 +0200

    payment object

---
+++ src/DeKoningAutoverhuur.Web.WebApplication/Common/Objects/PaymentObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeKoningAutoverhuur.Web.WebApplication.Common.Objects
+{
+    public class PaymentObject
+    {
+        public string name;
+        public string identifier;
+    }
+}
